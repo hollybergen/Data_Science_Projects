57ff5af469073cf82db0542da51caabb25b9dcfd
H Dub <hollyjbergen@gmail.com>
2019-04-04 00:49:08 -0700

    Updates to HW10

--- Homework10_Advanced_Data_Storage_and_Retrieval/Instructions/climate_app.py
+++ Homework10_Advanced_Data_Storage_and_Retrieval/Instructions/climate_app.py
@@ -6,6 +6,8 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 from flask import Flask, jsonify
+import datetime as dt
+from datetime import datetime
 
 #---------------- Set up Database -----------------#
 
@@ -41,7 +43,7 @@ def home():
 @app.route("/api/v1.0/precipitation")
 def precipitation():
     
-    results = session.query(Measurement, Station).filter(Measurement.station == Station.station).limit(10).all()
+    results = session.query(Measurement, Station).filter(Measurement.station == Station.station).all()
 
     precip_dict = {}
 
@@ -51,6 +53,39 @@ def precipitation():
 
     return jsonify(precip_dict)
     
+
+@app.route("/api/v1.0/stations")
+def stations():
+    stations = session.query(Station.name).all()
+    return jsonify(stations)
+
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+
+    max_date = session.query(Measurement.date).order_by(Measurement.date.desc()).limit(1).all()[0][0] # ('2017-08-23'), str
+    latest_date_dt = datetime.strptime(max_date,'%Y-%m-%d') # datetime.datetime(2017, 8, 23, 0, 0)
+    def yr_prior(target_date):
+            return datetime(year=target_date.year-1, month=target_date.month, day=target_date.day).strftime('%Y-%m-%d')
+    
+    station_yr_ago = yr_prior(latest_date_dt) # '2016-08-23', str
+
+    last_12 = session.query(Measurement.date, Measurement.tobs).\
+    filter(Measurement.date > f'{station_yr_ago}').\
+    filter(Measurement.date < f'{max_date}').\
+    order_by(Measurement.date.desc()).all()
+
+    date = []
+    tobs = []
+
+    for row in last_12:
+        date.append(row[0])
+        tobs.append(row[1])
+
+    tobs_dict = dict(zip(date, tobs))
+
+    return jsonify(tobs_dict)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
